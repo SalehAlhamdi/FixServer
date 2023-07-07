c84c1604b65f34239195206093ccde8bb3bbfc99
SalehAlhamdi <salehalhamd5@gmail.com>
2023-07-08 02:30:34 +0300

    Tool to fix the server

---
+++ FixServer/Fix Server/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Fix_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.proccessCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(154, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " عدد العمليات المعلقة";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // proccessCount
+            // 
+            this.proccessCount.AutoSize = true;
+            this.proccessCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.proccessCount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.proccessCount.Location = new System.Drawing.Point(115, 47);
+            this.proccessCount.Name = "proccessCount";
+            this.proccessCount.Size = new System.Drawing.Size(29, 31);
+            this.proccessCount.TabIndex = 2;
+            this.proccessCount.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(130, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "النظام بحالة ممتازة";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 211);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(480, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.Stretch = false;
+            this.statusStrip1.TabIndex = 6;
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(255, 17);
+            this.toolStripStatusLabel4.Text = "الحالة : جاهز                                                               ";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(133, 17);
+            this.toolStripStatusLabel1.Text = "                                          ";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(75, 17);
+            this.toolStripStatusLabel2.Text = "صالح الحامدي";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(467, 233);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.proccessCount);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fix Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label proccessCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ FixServer/Fix Server/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Fix_Server
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+        private System.Windows.Forms.Timer timer2 = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = (5000);
+            timer1.Tick += new EventHandler(showForms);
+            timer1.Start();
+            InitializeComponent();
+            timer2.Interval = (5000);
+            timer2.Tick += new EventHandler(fixProccess);
+            timer2.Start();
+        }
+        int count;
+
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showForms(sender, e);
+
+        }
+        private void showForms(object sender, EventArgs e)
+        {
+ 
+            Process[] allProcs = Process.GetProcesses();
+
+            count = 0;
+            foreach (Process proc in allProcs)
+            {
+                ProcessThreadCollection myThreads = proc.Threads;
+                if (proc.ProcessName == "cmd")
+                {
+                    count++;
+                }
+
+            }
+            proccessCount.Text = count.ToString();
+
+        }
+
+        private void fixProccess(object sender, EventArgs e)
+        {
+            Process[] workers = Process.GetProcessesByName("cmd");
+            if (workers.Length != 0)
+            {
+                foreach (Process worker in workers)
+                {
+                    count++;
+                    worker.Kill();
+                    worker.WaitForExit();
+                    worker.Dispose();
+                }
+            }
+
+        }
+
+    }
+}
